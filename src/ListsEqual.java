0048095519cd483dd554878a0325494acbb56636
Samantha Owusu-Antwi <samantha.owusuantwi@duke.edu>
2021-10-20 20:58:01 -0400

    Add documentation for ListsEquals.java

--- src/ListsEqual.java
+++ src/ListsEqual.java
@@ -1,86 +1,23 @@
 public class ListsEqual {
     public int equal(ListNode a1, ListNode a2) {
-        // APPROACH (non-recursion)
-        // initialize pointer to a1 current1
-        // iniitalize pointer to a2 current2
+        // 1/ EVALUATING POINTERS
+        // these first two if statements only work on the POINTERS to the first nodes
+        // we must use .info() to get the content of the actual nodes
+        // aka all actual nodes will not be null
 
-        // if one list is empty and the other is not return 0
-
-        // while current1 != null loop through its nodes
-            // if current1.info != current2.info return 0
-            // set current1 to current1.next and current2 to current2.next
-        // return 1
-
-        // EXECUTION (non-recursive)
-        // if one list is empty and the other is not return 0
-        /*
-        if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
-        // initialize pointer to a1 current1
-        ListNode current1 = a1;
-        // initalize pointer to a2 current2
-        ListNode current2 = a2;
-
-        // while current1 != null loop through its nodes
-        while(current1 != null && current2 != null) {
-            // if current1.info != current2.info return 0
-            if(current1.info != current2.info){return 0;}
-            // set current1 to current1.next and current2 to current2.next
-            current1 = current1.next;
-            current2 = current2.next;
-        }
-        // return 1
-        return 1;
-*/
-
-
-        // EXECUTION (recursive)
-        // if the next node in one of the Linkedlists is null and the next node in the other is not
-        // the LinkedLists are not equal sizes, return 0
-        /*if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
-        if((a1.next == null && a2.next != null) || (a1.next != null && a2.next == null)) return 0;
-
-        // only call equals if a1.info == a2.info
-        if(a1.info == a2.info) {
-            if(a1.next != null){equal(a1.next, a2.next);}
-        } else {
-            // otherwise return 0
-            return 0;
-        }
-
-        // if we make it this far that means all the elements are equal
-        return 1;*/
-
-
-        // if a1.info != a2.info return 0
-        // if a1.next != null && a2.next != null continue to check for equivalency
-            // if a1.info == a2.info call equal()
-            // return 1
-
-        /*ListNode current1 = a1;
-        ListNode current2 = a2;
-
-        if((current1 == null && current2 != null) || (current1 != null && current2 == null)) return 0;
-        if(current1 == null && current2 == null) return 1;
-
-        if(current1 != current2) return 0;
-        // while current1 != null loop through its nodes
-        while(current1.next != null || current2.next != null) {
-            current1 = current1.next;
-            current2 = current2.next;
-            // if current1.info != current2.info return 0
-            if(current1.info != current2.info){return 0;}
-            // set current1 to current1.next and current2 to current2.next
-
-        }
-
-        return 1;*/
-
+        // if both pointers nodes are null return 1
         if(a1 == null && a2 == null) return 1;
+        // if pointer is null and the other is not return 0 (the lists are unequal)
         if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
 
+        // 2. EVALUATING NODES
+        // if at least one list's node is not null (the other can be null, this would mean the lists are unequal)
         if(a1.next!=null || a2.next!=null){
+            // and the contents of the two nodes are the same, continue to call equal() on subsequent nodes
             if(a1.info == a2.info) return equal(a1.next, a2.next);
         }
+        // if we make it this far, that is calling equal() on all parallel nodes
+        // does not lead to a return of 0, return 1
         return 1;
 
     }
