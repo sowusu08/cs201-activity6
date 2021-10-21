a3a18faf6b8cb39a264853e325a5b63df476d182
Samantha Owusu-Antwi <samantha.owusuantwi@duke.edu>
2021-10-20 20:50:28 -0400

    Pass ListsEqual.java recursively

--- src/ListsEqual.java
+++ src/ListsEqual.java
@@ -1,29 +1,87 @@
 public class ListsEqual {
-    public int equal (ListNode a1, ListNode a2) {
-        // APPROACH
-        // if a1 == null && a2 != null || a1 != null && a2 == null return 0
-        // if a1.info == a2.info call equal(a1.next, a2.next)
+    public int equal(ListNode a1, ListNode a2) {
+        // APPROACH (non-recursion)
+        // initialize pointer to a1 current1
+        // iniitalize pointer to a2 current2
+
+        // if one list is empty and the other is not return 0
+
+        // while current1 != null loop through its nodes
+            // if current1.info != current2.info return 0
+            // set current1 to current1.next and current2 to current2.next
         // return 1
 
-        // EXECUTION
-        // if one of the lists is empty and the other is not return 0 (not equal)
+        // EXECUTION (non-recursive)
+        // if one list is empty and the other is not return 0
+        /*
         if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
-        // if both lists are empty return 1 (equal)
-        if(a1 == null && a2 == null) return 1;
+        // initialize pointer to a1 current1
+        ListNode current1 = a1;
+        // initalize pointer to a2 current2
+        ListNode current2 = a2;
 
-        // if a next node exists, call equals on the next node
-        if(a1.next != null) return equal(a1.next, a2.next);
-
-        // if contents of a1 and a2 are not the same return 0 and stop
-        if(a1.info != a2.info) return 0;
-
-        return 1;
-        // if a1.info == a2.info call equal(a1.next, a2.next)
-        /*if(a1.info == a2.info){
-            if(a1.next != null && a2.next != null) {
-                equal(a1.next, a2.next);
-            }
-        }*/
+        // while current1 != null loop through its nodes
+        while(current1 != null && current2 != null) {
+            // if current1.info != current2.info return 0
+            if(current1.info != current2.info){return 0;}
+            // set current1 to current1.next and current2 to current2.next
+            current1 = current1.next;
+            current2 = current2.next;
+        }
         // return 1
+        return 1;
+*/
+
+
+        // EXECUTION (recursive)
+        // if the next node in one of the Linkedlists is null and the next node in the other is not
+        // the LinkedLists are not equal sizes, return 0
+        /*if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
+        if((a1.next == null && a2.next != null) || (a1.next != null && a2.next == null)) return 0;
+
+        // only call equals if a1.info == a2.info
+        if(a1.info == a2.info) {
+            if(a1.next != null){equal(a1.next, a2.next);}
+        } else {
+            // otherwise return 0
+            return 0;
+        }
+
+        // if we make it this far that means all the elements are equal
+        return 1;*/
+
+
+        // if a1.info != a2.info return 0
+        // if a1.next != null && a2.next != null continue to check for equivalency
+            // if a1.info == a2.info call equal()
+            // return 1
+
+        /*ListNode current1 = a1;
+        ListNode current2 = a2;
+
+        if((current1 == null && current2 != null) || (current1 != null && current2 == null)) return 0;
+        if(current1 == null && current2 == null) return 1;
+
+        if(current1 != current2) return 0;
+        // while current1 != null loop through its nodes
+        while(current1.next != null || current2.next != null) {
+            current1 = current1.next;
+            current2 = current2.next;
+            // if current1.info != current2.info return 0
+            if(current1.info != current2.info){return 0;}
+            // set current1 to current1.next and current2 to current2.next
+
+        }
+
+        return 1;*/
+
+        if(a1 == null && a2 == null) return 1;
+        if((a1 == null && a2 != null) || (a1 != null && a2 == null)) return 0;
+
+        if(a1.next!=null || a2.next!=null){
+            if(a1.info == a2.info) return equal(a1.next, a2.next);
+        }
+        return 1;
+
     }
 }
